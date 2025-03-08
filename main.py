9cc8b62c110689e2d2a915755c835d497ab44011
danieldema <danieldema42@gmail.com>
2025-03-08 14:12:26 -0500

    added alerts. game is fully playable now

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import numpy as np
 import pygame
 import sys
 import math
+import pyautogui
+
 
 def create_board():
     board = np.zeros([6, 7])
@@ -15,13 +17,9 @@ def next_row(column: int):
 
 running = True
 
-print("Player 1, please enter your name:")
-player1 = input()
-print("Hello {}!".format(player1))
+player1 = pyautogui.prompt(text = 'Player 1, please enter your name:')
 
-print("Player 2, please enter your name:")
-player2 = input()
-print("Hello {}!".format(player2))
+player2 = pyautogui.prompt(text = 'Player 2, please enter your name:')
 
 board = create_board()
 
@@ -29,10 +27,11 @@ turn = 0
 
 pygame.init()
 
-screen = pygame.display.set_mode((700, 700))
+screen = pygame.display.set_mode((700, 800))
 pygame.display.set_caption('Connect Four')
 icon = pygame.image.load('connect_4_pic.jpg')
 pygame.display.set_icon(icon)
+myfont = pygame.font.SysFont("monospace", 75)
 
 for column in range(7):
     for row in range(6):
@@ -47,9 +46,9 @@ while running:
         if event.type == pygame.QUIT:
             sys.exit()
 
-        if not np.isin(0 , board):
-            print("Game over! Try again!")
-            break
+        if not np.isin(0, board):
+            pyautogui.alert(text = "Game over! Try again!")
+            running = False
 
         if event.type == pygame.MOUSEMOTION:
             pygame.draw.rect(screen, (0,0,0), (0,0, 700, 100))
@@ -65,87 +64,82 @@ while running:
 
                 move = int(math.floor(event.pos[0])/100)
 
-                while board[0, move] != 0:
-                    move = int(input("This column is full! Choose a different column."))
+                if board[0, move] == 0:
+                    board[next_row(move), move] = 1
+                    pygame.draw.circle(screen, (255,0,0), (move*100 + 50, (int(next_row(move)+2)*100) + 50), 45, 0)
+                    pygame.display.update()
+
+                    game_checker = False
                 
-                board[next_row(move), move] = 1
-                pygame.draw.circle(screen, (255,0,0), (move*100 + 50, (int(next_row(move)+2)*100) + 50), 45, 0)
-                pygame.display.update()
+                    for column in range(0, 4):
+                        for row in range(6):
+                            if board[row][column] == 1 and board[row][column+1] == 1 and board[row][column+2] == 1 and board[row][column+3] == 1:
+                                game_checker = True
 
-                print(move)
-                print(next_row(move))
-                print(board)
+                    for row in range(0, 3):
+                        for column in range(7):
+                            if board[row][column] == 1 and board[row+1][column] == 1 and board[row+2][column] == 1 and board[row+3][column] == 1:
+                                game_checker = True
 
-                game_checker = False
-                
-                for column in range(0, 4):
-                    for row in range(6):
-                        if board[row][column] == 1 and board[row][column+1] == 1 and board[row][column+2] == 1 and board[row][column+3] == 1:
-                            game_checker = True
+                    for column in range(4):
+                        for row in range(3, 6):
+                            if board[row][column] == 1 and board[row-1][column+1] == 1 and board[row-2][column+2] == 1 and board[row-3][column+3] == 1:
+                                game_checker = True
 
-                for row in range(0, 3):
-                    for column in range(7):
-                        if board[row][column] == 1 and board[row+1][column] == 1 and board[row+2][column] == 1 and board[row+3][column] == 1:
-                            game_checker = True
+                    for column in range(3, 7):
+                        for row in range(3, 6):
+                            if board[row][column] == 1 and board[row-1][column-1] == 1 and board[row-2][column-2] == 1 and board[row-3][column-3] == 1:
+                                game_checker = True
 
-                for column in range(4):
-                    for row in range(3, 6):
-                        if board[row][column] == 1 and board[row-1][column+1] == 1 and board[row-2][column+2] == 1 and board[row-3][column+3] == 1:
-                            game_checker = True
+                    if game_checker == True:
+                        pyautogui.alert(text = "Congratulations {}!".format(player1), title = "{} wins!".format(player1))
+                        running = False
+                    
+                    turn = 1
+                    print(board)
 
-                for column in range(3, 7):
-                    for row in range(3, 6):
-                        if board[row][column] == 1 and board[row-1][column-1] == 1 and board[row-2][column-2] == 1 and board[row-3][column-3] == 1:
-                            game_checker = True
-
-                if game_checker == True:
-                    print("Congratulations {}!".format(player1))
-                    break
-
-                turn = 1
+                if board[0, move] != 0 and turn == 0:
+                    pyautogui.alert("This column is full! Choose a different column!")
     
             else:
 
                 move = int(math.floor(event.pos[0]/100))
 
-                while move not in range(7):
-                    move = int(input("Your move needs to be a number between 0 and 6."))
 
-                while board[0, move] != 0:
-                    move = int(input("This column is full! Choose a different column."))
-        
-                board[next_row(move), move] = 2
-                pygame.draw.circle(screen, (255,255,0), (move*100 + 50, int((next_row(move)+2)*100) + 50), 45, 0)
-                pygame.display.update()
+                if board[0, move] == 0:
+                    board[next_row(move), move] = 2
+                    pygame.draw.circle(screen, (255,255,0), (move*100 + 50, (int(next_row(move)+2)*100) + 50), 45, 0)
+                    pygame.display.update()
 
+                    game_checker = False
 
-                print(board)
-
-                game_checker = False
-
-                for column in range(0, 4):
-                    for row in range(6):
-                        if board[row][column] == 2 and board[row][column+1] == 2 and board[row][column+2] == 2 and board[row][column+3] == 2:
-                            game_checker = True
+                    for column in range(0, 4):
+                        for row in range(6):
+                            if board[row][column] == 2 and board[row][column+1] == 2 and board[row][column+2] == 2 and board[row][column+3] == 2:
+                                game_checker = True
 
 
-                for row in range(0, 3):
-                    for column in range(7):
-                        if board[row][column] == 2 and board[row+1][column] == 2 and board[row+2][column] == 2 and board[row+3][column] == 2:
-                            game_checker = True
+                    for row in range(0, 3):
+                        for column in range(7):
+                            if board[row][column] == 2 and board[row+1][column] == 2 and board[row+2][column] == 2 and board[row+3][column] == 2:
+                                game_checker = True
 
-                for column in range(4):
-                    for row in range(3, 6):
-                        if board[row][column] == 2 and board[row-1][column+1] == 2 and board[row-2][column+2] == 2 and board[row-3][column+3] == 2:
-                            game_checker = True
+                    for column in range(4):
+                        for row in range(3, 6):
+                            if board[row][column] == 2 and board[row-1][column+1] == 2 and board[row-2][column+2] == 2 and board[row-3][column+3] == 2:
+                                game_checker = True
 
-                for column in range(3, 7):
-                    for row in range(3, 6):
-                        if board[row][column] == 2 and board[row-1][column-1] == 2 and board[row-2][column-2] == 2 and board[row-3][column-3] == 2:
-                            game_checker = True    
+                    for column in range(3, 7):
+                        for row in range(3, 6):
+                            if board[row][column] == 2 and board[row-1][column-1] == 2 and board[row-2][column-2] == 2 and board[row-3][column-3] == 2:
+                                game_checker = True    
 
-                if game_checker == True:
-                    print("Congratulations {}!".format(player2))
-                    break
+                    if game_checker == True:
+                        pyautogui.alert(text = "Congratulations {}!".format(player2), title = "{} wins!".format(player2))
+                        running = False
 
-                turn = 0
+                    turn = 0
+                    print(board)
+
+                if board[0, move] != 0 and turn == 1:
+                    pyautogui.alert("This column is full! Choose a different column!")
